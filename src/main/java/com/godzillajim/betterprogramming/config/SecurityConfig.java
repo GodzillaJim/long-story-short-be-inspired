08e4d0d9311104d910da67aa8f0b072a5e765706
godzillajim <jimnanjoroge76@gmail.com>
2022-02-02 15:02:45 +0300

    (feat)Deploy

--- src/main/java/com/godzillajim/betterprogramming/config/SecurityConfig.java
+++ src/main/java/com/godzillajim/betterprogramming/config/SecurityConfig.java
@@ -55,4 +55,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
+
 }
